28a900b15c9ce86b803101b8e8af10fe21f2caa6
Pekka Aaltonen <arx@iki.fi>
2024-06-16 19:00:59 +0300

    Add option for 10ms blue window

--- Scripts/SL-PlayerProfiles.lua
+++ Scripts/SL-PlayerProfiles.lua
@@ -65,9 +65,10 @@ local permitted_profile_settings = {
 	ErrorBarMultiTick    = "boolean",
 	ErrorBarTrim         = "string",
 
-	ShowFaPlusWindow     = "boolean",
-	ShowEXScore          = "boolean",
-	ShowFaPlusPane       = "boolean",
+	ShowFaPlusWindow       = "boolean",
+	ShowEXScore            = "boolean",
+	ShowFaPlusPane         = "boolean",
+	TighterFantasticWindow = "boolean",
 
 	HideEarlyDecentWayOffJudgments = "boolean",
 	HideEarlyDecentWayOffFlash     = "boolean",
