482e01c1b81e8e60de906d3b982d4f57fde337fb
vide <git@vide.foo>
2025-03-28 13:44:08 +0200

    [proto] display ex score in song wheel
    Unify EX score calculations
    
    Also hide Lamp when showing EX
    
    (cherry picked from commit 9fecf6f377318c76597af79ba648e4a69337064a)

--- Scripts/99 SL-ThemePrefs.lua
+++ Scripts/99 SL-ThemePrefs.lua
@@ -344,6 +344,12 @@ SL_CustomPrefs.Get = function()
 			Default = false,
 			Choices =  { THEME:GetString("ThemePrefs","Yes"), THEME:GetString("ThemePrefs", "No") },
 			Values = { true, false }
+		},
+
+		MusicWheelEXScore = {
+			Default = false,
+			Choices =  { THEME:GetString("ThemePrefs","Yes"), THEME:GetString("ThemePrefs", "No") },
+			Values = { true, false }
 		}
 	}
 end
